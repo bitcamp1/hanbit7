c0be3e38673e1cb9e4003e81f0e28b4355dc4ae9
Administrator <Administrator@59.12.71.212>
2015-02-16 23:00:03 +0900

    fail mapper

--- hanbit7/src/main/java/com/hanbit/web/service/GuestService.java
+++ hanbit7/src/main/java/com/hanbit/web/service/GuestService.java
@@ -1,40 +1,13 @@
 package com.hanbit.web.service;
 
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.hanbit.web.dao.GuestDao;
-import com.hanbit.web.vo.GuestVo;
-
-public class GuestService implements GuestDao{
-
-	@Override
-	public List<GuestVo> getGuestList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public GuestVo getGuest(int id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addGuest(GuestVo guest) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void updateGuest(GuestVo guest) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteGuest(int guestNo) {
-		// TODO Auto-generated method stub
-		
-	}
+@Service
+public class GuestService {
+@Autowired
+private GuestDao mapper;
+	
 
 }
