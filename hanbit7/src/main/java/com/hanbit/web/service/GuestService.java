22a1b8423ca556180150a9c3464d414023b25670
jkpark <jkpark3070@gmail.com>
2015-02-17 09:36:20 +0900

    classpath*:/persistence/*Mapper.xml

--- hanbit7/src/main/java/com/hanbit/web/service/GuestService.java
+++ hanbit7/src/main/java/com/hanbit/web/service/GuestService.java
@@ -1,13 +1,9 @@
 package com.hanbit.web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import com.hanbit.web.dao.GuestDao;
-@Service
+
 public class GuestService {
-@Autowired
-private GuestDao mapper;
+
 	
 
 }
